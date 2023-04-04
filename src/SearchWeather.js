863bab976b39a7bb60797d417599082c000dcf71
abhipsaadya <129159833+abhipsaadya@users.noreply.github.com>
2023-04-04 09:37:11 +0530

    Update SearchWeather.js

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -50,6 +50,37 @@ const SearchWeather = () => {
   } else {
     return <div>Loading....</div>;
   }
+  
+   let img = null;
+  if (typeof data.main != "undefined") {
+    if (data.weather[0].main == "Clouds") {
+      img =
+        "https://images.unsplash.com/photo-1513069020900-a162c4db0762?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNsb3VkcyUyMHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Thunderstrom") {
+      img =
+        "https://images.unsplash.com/photo-1429552077091-836152271555?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGlnaHRpbmclMjBzdG9ybXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Drizzle") {
+      img =
+        "https://images.unsplash.com/photo-1630574232726-fc3ea90637b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZHJpenpsaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Snow") {
+      img =
+        "https://images.unsplash.com/photo-1431036101494-66a36de47def?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c25vd2ZhbGx8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Haze") {
+      img =
+        "https://images.unsplash.com/photo-1613908614131-77aed27b030b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhhemV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Rain") {
+      img =
+        "https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFpbiUyMHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60";
+    } else if (data.weather[0].main === "Smoke") {
+      img =
+        "https://images.unsplash.com/photo-1639540052028-114587ade923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNtb2tlJTIwd2VhdGhlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60";
+    } else {
+      img =
+        "https://images.unsplash.com/photo-1678338712030-6b529c30bd41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDg4fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60";
+    }
+  } else {
+    return <div>Loading....</div>;
+  }
 
   let temp = (data.main.temp - 273.15).toFixed(2);
   let temp_min = (data.main.temp_min - 273.15).toFixed(2);
@@ -82,13 +113,11 @@ const SearchWeather = () => {
            <div className=" text-center mb-4 ">
             <h1 className="name">Weather App</h1>
           </div>
-          <div className="col-md-4">
+          
+               <div className="col-md-4">
             <div className="card text-white text-center border-0">
-              <img
-                src="https://images.unsplash.com/photo-1500390365106-166bb67248d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                className="card-img"
-                alt="..."
-              />
+              {<img src={img} className="card-img" alt="..." />}
+  
               <div className="card-img-overlay">
                 <form onSubmit={handleSubmit}>
                   <div className="input-group mb-4 w-75 mx-auto">
